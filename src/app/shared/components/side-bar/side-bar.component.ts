17a707250b9648c46518cb76d1515bf57c860327
SFMZ21 <smartinezz1@miumg.edu.gt>
2022-10-15 02:33:17 -0600

    Creación de componentes para menú

--- src/app/shared/components/side-bar/side-bar.component.ts
+++ src/app/shared/components/side-bar/side-bar.component.ts
@@ -7,9 +7,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideBarComponent implements OnInit {
 
+  mainMenu:{
+    defaultOptions: Array<any>,
+    accessLink: Array<any>
+  } = {
+    defaultOptions: [], accessLink:[]}
+
+    customOptions: Array<any> =[]
+
   constructor() { }
 
   ngOnInit(): void {
+    this.mainMenu.defaultOptions=[
+      {name:'Home',
+       icon:'uil uil-estate',
+       router:['/']
+      },
+      {name:'Buscar',
+       icon:'uil uil-search',
+       router:['/', 'history']
+      },
+      {name:'Tu biblioteca',
+       icon:'uil uil-chart',
+       router:['/', 'favorites']
+      }
+    ]
+
+    this.mainMenu.accessLink=[
+      {name:'Crear Lista',
+       icon:'uil-plus-square'
+      },
+      {name:'Canciones que te gustan',
+       icon:'uil-heart-medical'
+      }
+    ]
+
+    this.customOptions=[
+      {name:'Mi lista 1',
+       router:['/']
+      },
+      {name:'Mi lista 2',
+       router:['/']
+      },
+      {name:'Mi lista 3',
+       router:['/']
+      },
+      {name:'Mi lista 4',
+       router:['/']
+      },
+      
+    ]
   }
 
 }
