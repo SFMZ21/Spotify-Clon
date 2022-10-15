49c0560d79caa84f0fc007dcb4730d371a6dc229
SFMZ21 <smartinezz1@miumg.edu.gt>
2022-10-15 01:16:33 -0600

    Agregando rutas de home

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,8 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    
+    path:'', //localhost:4200/
+    loadChildren: ()=> import(`./modules/home/home.module`).then(m => m.HomeModule)
   }
 ];
 
