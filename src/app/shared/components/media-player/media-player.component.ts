b65355c2453c1487e271a2918c38e8c98c7cd1eb
SFMZ21 <smartinezz1@miumg.edu.gt>
2022-10-23 02:15:07 -0600

    Agragando NgTemplate para el reproductor del media player

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -7,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaPlayerComponent implements OnInit {
 
+  mockCover: any ={
+    cover:'https://th.bing.com/th/id/R.2648234494131b8a77efcf78c5d0f74b?rik=COGJte%2fU02YYEQ&riu=http%3a%2f%2fauditorio-telmex.com%2farchivos%2fevento694%2fimagen_mediana.jpg&ehk=5R9c5JXvYUvXJKdnB0NgFh5MiqFfTy41nhmNGRu92NQ%3d&risl=&pid=ImgRaw&r=0',
+    album:'Balas Perdidas',
+    name: 'Besos en Guerra'
+  }
   constructor() { }
 
   ngOnInit(): void {
