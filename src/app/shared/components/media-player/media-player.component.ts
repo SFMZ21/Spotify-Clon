30aaeeafe0a2306f850ef9126e932470a3780bce
SFMZ21 <smartinezz1@miumg.edu.gt>
2022-10-26 17:47:58 -0600

    Estructura de card player

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TracksModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-media-player',
@@ -7,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MediaPlayerComponent implements OnInit {
 
-  mockCover: any ={
+  mockCover: TracksModel ={
     cover:'https://th.bing.com/th/id/R.2648234494131b8a77efcf78c5d0f74b?rik=COGJte%2fU02YYEQ&riu=http%3a%2f%2fauditorio-telmex.com%2farchivos%2fevento694%2fimagen_mediana.jpg&ehk=5R9c5JXvYUvXJKdnB0NgFh5MiqFfTy41nhmNGRu92NQ%3d&risl=&pid=ImgRaw&r=0',
     album:'Balas Perdidas',
-    name: 'Besos en Guerra'
+    name: 'Besos en Guerra',
+    url:'http://localhost/tracks.mp3',
+    _id:1
   }
   constructor() { }
 
