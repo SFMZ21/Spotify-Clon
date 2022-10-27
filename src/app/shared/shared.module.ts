51f42a31f5425d06baa7827bb5b3f0dd13b7aa39
SFMZ21 <smartinezz1@miumg.edu.gt>
2022-10-27 17:27:25 -0600

    Agregando directivas,manejo de errores en imagenes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,6 +7,7 @@ import { CardPlayerComponent } from './components/card-player/card-player.compon
 import { SectionGenericComponent } from './components/section-generic/section-generic.component';
 import { PlayListBodyComponent } from './components/play-list-body/play-list-body.component';
 import { PlayListHeaderComponent } from './components/play-list-header/play-list-header.component';
+import { ImgBrokenDirective } from './directives/img-broken.directive';
 
 
 
@@ -18,7 +19,8 @@ import { PlayListHeaderComponent } from './components/play-list-header/play-list
     CardPlayerComponent,
     SectionGenericComponent,
     PlayListBodyComponent,
-    PlayListHeaderComponent
+    PlayListHeaderComponent,
+    ImgBrokenDirective
   ],
   imports: [
     CommonModule
@@ -30,7 +32,8 @@ import { PlayListHeaderComponent } from './components/play-list-header/play-list
     CardPlayerComponent,
     SectionGenericComponent,
     PlayListBodyComponent,
-    PlayListHeaderComponent
+    PlayListHeaderComponent,
+    ImgBrokenDirective
   ]
 })
 export class SharedModule { }
