522edf8afba322cc7bf784971faf511f2bf47f75
SFMZ21 <smartinezz1@miumg.edu.gt>
2022-11-03 02:23:12 -0600

    Agregar cookies al login

--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '@modules/auth/services/auth.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-auth-page',
@@ -8,9 +9,10 @@ import { AuthService } from '@modules/auth/services/auth.service';
   styleUrls: ['./auth-page.component.css']
 })
 export class AuthPageComponent implements OnInit {
+  errorSesion: boolean = false;
   formLogin: FormGroup = new FormGroup({});
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private cookie:CookieService) { }
 
   ngOnInit(): void {
     this.formLogin = new FormGroup(
@@ -30,6 +32,16 @@ export class AuthPageComponent implements OnInit {
   sendLogin(): void{
     const {email,password} = this.formLogin.value
     this.authService.sendCredentials(email,password)
+    .subscribe(responseOk =>{
+      console.log('Inicio de sesion correcta');
+      const {tokenSesion,data} =responseOk
+      this.cookie.set('token', tokenSesion,4,'/')
+    },
+    err =>{
+      this.errorSesion = true
+      console.log('Ocurrio un error con tu email o password');
+
+    })
   }
 
 }
